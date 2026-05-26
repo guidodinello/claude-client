b4323b66310535e71bcfc11f7ae94b1f78b0b54e
Guido Dinello <guido.dinello@gmail.com>
2026-05-25 22:20:10 -0300

    chore(exports): re-export new conversation TypedDicts from package __init__

--- claude_client/__init__.py
+++ claude_client/__init__.py
@@ -1,15 +1,28 @@
 from .client import ClaudeClient
 from .exceptions import AuthError, NotFoundError, UploadError
-from .models import ConversationDict, DocDict, MemoryDict, Page, ProjectDict, ProjectExport
+from .models import (
+    ChatMessageDict,
+    ConversationDetailDict,
+    ConversationDict,
+    DocDict,
+    MemoryDict,
+    MessageContentDict,
+    Page,
+    ProjectDict,
+    ProjectExport,
+)
 
 __all__ = [
     "ClaudeClient",
     "AuthError",
     "NotFoundError",
     "UploadError",
+    "ChatMessageDict",
+    "ConversationDetailDict",
     "ConversationDict",
     "DocDict",
     "MemoryDict",
+    "MessageContentDict",
     "Page",
     "ProjectDict",
     "ProjectExport",
